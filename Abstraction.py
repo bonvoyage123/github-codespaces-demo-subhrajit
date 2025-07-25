82f0b4b0cd619df0d2d676727230a95db8fbe86c
Subhrajit <49794369+bonvoyage123@users.noreply.github.com>
2025-07-25 22:57:44 +0530

    Add files via upload

--- Abstraction.py
+++ Abstraction.py
@@ -10,7 +10,10 @@ class MyClass:
         If we use "__age"", that will not trigger an error """
 
 p1 = MyClass("Subhrajit",35)
-#p1.info()
+print ("Accessing Variables through Method")
+p1.info()
+
+print ("Accessing Variables through instances")
 print (p1.name)
 #print (p1.__age)
 """AttributeError: 'MyClass' object has no attribute '__age'. This is because self.__age in the constructor is stored as p1._MyClass__age, not as p1.__age.
@@ -18,4 +21,3 @@ This is called name mangling"""
 print (p1._MyClass__age)  # name mangling :: Not a recommended approach, doesn't solve the purpose of privacy
 """name mangling :: object._class__private variable.
 When we initialize, self.__age, Python internally changes the attribute’s name to _MyClass__age"""
-print (p1.__age)
